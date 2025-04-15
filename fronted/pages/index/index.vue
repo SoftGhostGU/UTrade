97f5549c1e671cf3c965e27a47231492033729d1
Taslr2 <19535838578@163.com>
2025-04-15 17:22:32 +0800

    引入uview组件库

--- fronted/pages/index/index.vue
+++ fronted/pages/index/index.vue
@@ -1,52 +1,103 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+    <view class="content">
+        <view class="top-section">
+        </view>
+		
+        <view class="content-section">
+            <div class="app-name">U Trade!</div>
+            <div class="app-desc">大学生技能交换市场</div>
+            <button class="enter-btn">进入</button>
+        </view>
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+    export default {
+        data() {
+            return {
+                title: 'U Trade!'
+            }
+        },
+        onLoad() {
+        },
+        methods: {
+        }
+    }
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+    .content {
+        height: 100vh;
+        position: relative;
+        background-color: #ffffff;
+    }
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
+    .top-section {
+        height: 40%;
+        background: linear-gradient(180deg, #47acf0 0%, #2196f3 100%);
+        position: relative;
+    }
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+    .wave {
+        position: absolute;
+        bottom: -2px;
+        left: 0;
+        width: 100%;
+        line-height: 0;
+    }
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+    .wave svg {
+        position: relative;
+        display: block;
+        width: 100%;
+        height: 100px;
+    }
+	.content-section {
+    position: absolute;
+    top: 45%; /* 将 50% 改小一些 */
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+}
+
+    .logo {
+        width: 160rpx;
+        height: 160rpx;
+        margin-bottom: 30rpx;
+    }
+
+    .app-name {
+        font-size: 48rpx;
+        color: #333333;
+        font-weight: bold;
+		padding-top: 350px;
+		box-sizing: content-box;
+    }
+
+    .app-desc {
+        font-size: 32rpx;
+        color: #666666;
+        padding-bottom: 250px;
+		box-sizing:content-box;
+    }
+
+    .enter-btn {
+        width: 280rpx;
+        height: 80rpx;
+        line-height: 80rpx;
+        border-radius: 40rpx;
+        background: linear-gradient(90deg, #47acf0 0%, #2196f3 100%);
+        color: #ffffff;
+        font-size: 32rpx;
+        text-align: center;
+        box-shadow: 0 4rpx 16rpx rgba(33, 150, 243, 0.3);
+    }
+
+    .enter-btn:active {
+        transform: scale(0.98);
+        box-shadow: 0 2rpx 8rpx rgba(33, 150, 243, 0.3);
+    }
 </style>
