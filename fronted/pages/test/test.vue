66a53f4d8280ab47f2195d37ba788d004379fb75
Taslr2 <19535838578@163.com>
2025-04-15 21:39:22 +0800

    完成首页的动画效果

---
+++ fronted/pages/test/test.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <view class="content">
+
+    </view>
+</template>
+
+
+<style scoped>
+    .content {
+        width: 100vw;
+        height: 100vh;
+        background-color: aqua;
+    }
+</style>
