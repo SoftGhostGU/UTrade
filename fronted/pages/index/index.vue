d2c3117432f6a8465704326ab12ec6f6387209ef
Taslr2 <19535838578@163.com>
2025-04-15 18:29:19 +0800

    完成首页编写

--- fronted/pages/index/index.vue
+++ fronted/pages/index/index.vue
@@ -2,41 +2,62 @@
     <view class="content">
         <view class="top-section">
         </view>
-		
+		<view class="circle-section1"></view>
+		<view class="circle-section2"></view>
         <view class="content-section">
-            <div class="app-name">U Trade!</div>
-            <div class="app-desc">大学生技能交换市场</div>
-            <button class="enter-btn">进入</button>
+			<div class="logo"></div>
+            <div class="app-name">{{app_name}}</div>
+            <div class="app-desc">{{app_desc}}</div>
+            <button class="enter-btn">{{enter_btn_text}}</button>
         </view>
     </view>
 </template>
 
-<script>
-    export default {
-        data() {
-            return {
-                title: 'U Trade!'
-            }
-        },
-        onLoad() {
-        },
-        methods: {
-        }
-    }
+<script setup>
+const app_name='U Trade!';
+const app_desc="大学生技能交换市场";
+const enter_btn_text="进入";
 </script>
 
 <style>
-    .content {
+ .content {
         height: 100vh;
         position: relative;
         background-color: #ffffff;
+        width: 100%; 
     }
 
-    .top-section {
-        height: 40%;
+	.top-section {
+        height: 50%;
         background: linear-gradient(180deg, #47acf0 0%, #2196f3 100%);
         position: relative;
     }
+.circle-section1,
+.circle-section1 {
+	position: absolute;
+	left: 50%;
+    top: 76%;
+    transform: translate(-50%, -50%);
+	width: 750px;
+	height: 800px;
+	background-color: white;
+	z-index: 0;
+	border-radius: 100%;
+}
+	.logo{
+		width: 120px;
+		height: 120px;
+		background-color: white;
+		background-image: url('/static/U_logo.png');
+		background-repeat: no-repeat;
+		background-position: center;
+		position: absolute;
+		left: 50%;
+    	top: 35%;
+    	transform: translate(-50%, -50%);
+		border-radius: 50rpx;
+		box-shadow:  5rpx 8rpx 10rpx rgba(22, 22, 22, 0.1);
+	}
 
     .wave {
         position: absolute;
@@ -54,7 +75,7 @@
     }
 	.content-section {
     position: absolute;
-    top: 45%; /* 将 50% 改小一些 */
+    top: 45%; 
     left: 50%;
     transform: translate(-50%, -50%);
     display: flex;
@@ -63,35 +84,33 @@
     width: 100%;
 }
 
-    .logo {
-        width: 160rpx;
-        height: 160rpx;
-        margin-bottom: 30rpx;
-    }
-
     .app-name {
-        font-size: 48rpx;
+        font-size: 75rpx;
         color: #333333;
         font-weight: bold;
 		padding-top: 350px;
-		box-sizing: content-box;
+		
+
     }
 
     .app-desc {
         font-size: 32rpx;
-        color: #666666;
+        color: #949494;
+		padding-top:30rpx;
         padding-bottom: 250px;
-		box-sizing:content-box;
+
+
     }
 
     .enter-btn {
-        width: 280rpx;
-        height: 80rpx;
-        line-height: 80rpx;
-        border-radius: 40rpx;
-        background: linear-gradient(90deg, #47acf0 0%, #2196f3 100%);
-        color: #ffffff;
-        font-size: 32rpx;
+        width: 300rpx;
+        height: 100rpx;
+        line-height: 100rpx;
+        border-radius: 60rpx;
+        background: linear-gradient(90deg, #45aefd 0%, #5160ff 100%);
+        color: #eeeeee;
+        font-size: 36rpx;
+		font-weight: 600;
         text-align: center;
         box-shadow: 0 4rpx 16rpx rgba(33, 150, 243, 0.3);
     }
