677c062265e2a4d1181b552a4e7f8e9ab29c5bb8
GHOST <3089308393@qq.com>
2025-04-25 09:45:20 +0800

    用户注册后端（后续改为wx接口）

--- backend/src/main/java/com/ghost/backend/service/UserService.java
+++ backend/src/main/java/com/ghost/backend/service/UserService.java
@@ -1,4 +1,9 @@
 package com.ghost.backend.service;
 
+import com.ghost.backend.pojo.User;
+
 public interface UserService {
+    User findByUsername(String username);
+
+    void register(String username, String password, String email);
 }
